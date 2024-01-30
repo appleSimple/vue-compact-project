d2516ed10a46927dea44e9870fb4303e9a25a8f8
appleSimple <jy_4148@naver.com>
2024-01-31 08:57:05 +0900

    feat : add up components on center

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import ImageBlock from './components/ImageBlock.vue';
+import StopWatch from './components/StopWatch.vue';
+import CubeIcon from './icons/CubeIcon.vue';
+
+const blockData = {
+  imgUrl: '/vite.svg',
+  icon: CubeIcon,
+  title: '타이틀',
+  description: '디스크립션입니다.디스크립션입니다.디스크립션입니다.디스크립션입니다.',
+};
+
+const stopWatch = ref<InstanceType<typeof StopWatch> | null>(null);
+
+onMounted(() => {
+  stopWatch.value?.startWatch();
+});
+
+function pauseWatch() {
+  stopWatch.value?.pauseWatch();
+}
+
+function reStartWatch() {
+  stopWatch.value?.reStartWatch();
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <StopWatch ref="stopWatch" />
+    <div>
+      <button type="button" @click="pauseWatch">일시정지</button>
+      <button type="button" @click="reStartWatch">다시시작</button>
+    </div>
+    <ImageBlock
+      :img-url="blockData.imgUrl"
+      :icon="blockData.icon"
+      :title="blockData.title"
+      :description="blockData.description"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style lang="scss" scoped></style>
