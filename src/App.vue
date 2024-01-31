c24c2994715d733f71854fba0fb573fc50285dc4
appleSimple <jy_4148@naver.com>
2024-01-31 23:11:00 +0900

    refactor :  stopWatch feature and list data feature

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import ImageBlock from './components/ImageBlock.vue';
 import StopWatch from './components/StopWatch.vue';
-import CubeIcon from './icons/CubeIcon.vue';
-
-const blockData = {
-  imgUrl: '/vite.svg',
-  icon: CubeIcon,
-  title: '타이틀',
-  description: '디스크립션입니다.디스크립션입니다.디스크립션입니다.디스크립션입니다.',
-};
+import { list } from './data/list';
 
 const stopWatch = ref<InstanceType<typeof StopWatch> | null>(null);
-
-onMounted(() => {
-  stopWatch.value?.startWatch();
-});
 
 function pauseWatch() {
   stopWatch.value?.pauseWatch();
@@ -33,12 +22,14 @@ function reStartWatch() {
       <button type="button" @click="pauseWatch">일시정지</button>
       <button type="button" @click="reStartWatch">다시시작</button>
     </div>
-    <ImageBlock
-      :img-url="blockData.imgUrl"
-      :icon="blockData.icon"
-      :title="blockData.title"
-      :description="blockData.description"
-    />
+    <div v-for="(element, index) in list" :key="index">
+      <ImageBlock
+        :img-url="element.imgUrl"
+        :icon="element.icon"
+        :title="element.title"
+        :description="element.description"
+      />
+    </div>
   </div>
 </template>
 
