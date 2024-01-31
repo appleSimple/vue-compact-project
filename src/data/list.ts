5dd4f12226db36a14240444e8ce167484aa3157d
appleSimple <jy_4148@naver.com>
2024-01-31 23:09:02 +0900

    feat : add list mock data

---
+++ src/data/list.ts
@@ -0,0 +1,26 @@
+import { ListElement } from '../types/listElement';
+import CubeIcon from '../icons/CubeIcon.vue';
+
+export const list: ListElement[] = [
+  {
+    imgUrl:
+      'https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=1350',
+    icon: CubeIcon,
+    title: '타이틀',
+    description: '디스크립션입니다.디스크립션입니다.디스크립션입니다.디스크립션입니다.',
+  },
+  {
+    imgUrl:
+      'https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=1350',
+    icon: CubeIcon,
+    title: '타이틀',
+    description: '디스크립션입니다.디스크립션입니다.디스크립션입니다.디스크립션입니다.',
+  },
+  {
+    imgUrl:
+      'https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=1350',
+    icon: CubeIcon,
+    title: '타이틀',
+    description: '디스크립션입니다.디스크립션입니다.디스크립션입니다.디스크립션입니다.',
+  },
+];
