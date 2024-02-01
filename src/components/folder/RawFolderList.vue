c156f5e03f17ce90ddc21d5228df5db0bba0ca3b
appleSimple <jy_4148@naver.com>
2024-02-01 21:29:41 +0900

    feat : create tree list

---
+++ src/components/folder/RawFolderList.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { FolderType } from '../../types/folder';
+
+const props = defineProps({
+  folderList: {
+    type: Array as PropType<FolderType[]>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <ul v-for="(folder, index) in props.folderList" :key="`${folder.id}-${index}`">
+    <li :folder="folder">{{ folder.title }} / id: {{ folder.id }} / depth: {{ folder.depth }} / parent: {{ folder.parent || 'null' }}</li>
+    <div v-if="folder.children.length > 0">
+      <RawFolderList :folderList="folder.children" />
+    </div>
+  </ul>
+</template>
